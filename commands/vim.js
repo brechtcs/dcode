4e2d062a03033713bcc5440396db6ea3cff8721f
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2019-04-10 20:51:04 +0200

    Use hashbase URL for dterm dependencies

--- commands/vim.js
+++ commands/vim.js
@@ -1,4 +1,4 @@
-import joinPath from 'dat://f7a6cde10e31b4244ebe9e6aa42ff8dfe6b417730852bd59e8030df4f9987b26/modules/join-path.js'
+import joinPath from 'dat://dterm.hashbase.io/modules/join-path.js'
 
 export default function (opts, location) {
   var origin = new URL(import.meta.url).origin
--- modules/dcode-file.js
+++ modules/dcode-file.js
@@ -1,5 +1,5 @@
-import assert from 'dat://f7a6cde10e31b4244ebe9e6aa42ff8dfe6b417730852bd59e8030df4f9987b26/modules/assert.js'
-import parsePath from 'dat://f7a6cde10e31b4244ebe9e6aa42ff8dfe6b417730852bd59e8030df4f9987b26/modules/dterm-parse-path.js'
+import assert from 'dat://dterm.hashbase.io/modules/assert.js'
+import parsePath from 'dat://dterm.hashbase.io/modules/dterm-parse-path.js'
 
 const EXT = /\.[\w]+$/
 const SEP = '/'
@@ -7,34 +7,34 @@ const SEP = '/'
 export default class DcodeFile {
   constructor (pathname) {
     var {archive, key, path} = parsePath(pathname)
-    
+
     this.archive = archive
     this.key = key
     this.path = path
     this.unsaved = false
   }
-  
+
   async open () {
     return this.archive.readFile(this.path)
   }
-  
+
   async save (contents) {
     this.unsaved = false
     return this.archive.writeFile(this.path, contents)
   }
-  
+
   get base () {
     return this.path.split(SEP).pop()
   }
-  
+
   get dir () {
     return this.path.split(SEP).slice(0, -1).join(SEP)
   }
-  
+
   get ext () {
     return this.path.match(EXT)[0]
   }
-  
+
   get mime () {
     switch (this.ext) {
       case '.css': return 'text/css'
@@ -43,7 +43,7 @@ export default class DcodeFile {
       default: return 'text/plain'
     }
   }
-  
+
   get name () {
     return this.base.replace(this.ext, '')
   }
